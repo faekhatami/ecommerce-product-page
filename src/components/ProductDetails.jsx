733345d052e535c9e21a48c267aa74a5eb986acb
faekhatami <faekhatami@gmail.com>
2024-07-09 14:12:41 +0330

    update

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const ProductDetails = ({ productId, onAddToCart }) => {
   const [product, setProduct] = useState(null);
