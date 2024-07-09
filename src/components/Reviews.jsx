8ca0df5d32188f77b11097af1a20a3f5321bce86
faekhatami <faekhatami@gmail.com>
2024-07-09 10:37:19 +0330

    reviews

---
+++ src/components/Reviews.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Reviews = ({ reviews }) => {
+  return (
+    <div className="reviews">
+      <h2>Customer Reviews</h2>
+      {reviews.map((review) => (
+        <div key={review.id} className="review">
+          <p>
+            <strong>{review.author}</strong>
+          </p>
+          <p>{review.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Reviews;
